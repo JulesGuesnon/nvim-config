931975eac961c2e52441e2bd18b5db2e3c119d11
JulesGuesnon <jules.guesnon@hetic.net>
2023-11-05 11:14:43 +0900

    fix(Plugins): Set default color scheme and fixed telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,21 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "%.git/" },
+      },
+    },
     keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
       {
         "<leader><Space>",
-        function() require("telescope.builtin").find_files({
-           cwd = vim.fn.getcwd(),
-           hidden = true,
-        }) end,
+        function()
+          require("telescope.builtin").find_files({
+            hidden = true,
+          })
+        end,
         desc = "Find Files (cwd)",
       },
     },
   },
 }
-

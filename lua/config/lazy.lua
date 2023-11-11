8be6bfdb7d9404a13e9d7eafd4bba609f50c4124
JulesGuesnon <jules.guesnon@hetic.net>
2023-11-11 13:08:42 +0900

    feat(Color): Customized scheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
+    { dir = "plugins/test" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
@@ -28,7 +29,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  install = {},
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,44 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "night",
+      on_colors = function(colors)
+        colors.bg = "#0e1116"
+        colors.bg_dark = "#050608"
+        colors.bg_sidebar = "#050608"
+      end,
+      on_highlights = function(hl, _c)
+        local purple = "#9990e0"
+        hl["@type.builtin"] = {
+          fg = purple,
+        }
+        hl["@type"] = {
+          fg = purple,
+        }
+        hl.IlluminatedWordRead = {
+          fg = purple,
+        }
+        hl.IlluminatedWordText = {
+          fg = purple,
+        }
+        hl.IlluminatedWordWrite = {
+          fg = purple,
+        }
+        hl["@lsp.type.struct"] = {
+          link = "@type",
+        }
+        hl["@lsp.type.typeAlias"] = {
+          link = "@type",
+        }
+      end,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-storm",
+      colorscheme = "tokyonight",
     },
   },
 }
